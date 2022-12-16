456b92bb87cb776dda5c516cb9abd4f845c43d60
jonatan-student <78746701+jonatan-student@users.noreply.github.com>
2022-12-16 23:55:34 +0100

    0.1-0.6 done 0.5 0-14 need to be redone cux i dum

--- NsPostProscessing.py
+++ NsPostProscessing.py
@@ -3,15 +3,14 @@ import matplotlib.pyplot as plt
 import os
 from sklearn.cluster import KMeans
 
-_Directory = "NS01dalet"
+_Directory = "NS06dalet"
 
 if not os.path.exists(_Directory):
     os.mkdir(_Directory)
 
-T = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]#, 5.5, 6, 6.5, 7, 7.5, 8, 8.5,9,9.5,10 ,10.5, 11,11.5,12,12.5,13,13.5,14,14.5,15,15.5,16,16.5,17,17.5,18,18.5,19,19.5,20,20.5,21,21.5,22,22.5,23,23.5,24,24.5,25,25.5,26,26.5,27,27.5,28,28.5,29,29.5,30,30.5,31,31.5,32,32.5,33,33.5,34,34.5,35,35.5,36,36.5,37,37.5,38,38.5,39,39.5,40,40.5,41,41.5,42,42.5,43,43.5,44,44.5,45,45.5,46,46.5,47,47.5,48,48.5,49,49.5,50]
 list_of_allPeaks = []
 
-for Time in range(51):
+for Time in range(101):
     print(Time)
     Vorticity_data=np.genfromtxt(f'Nstest/Vorticity_atTime-{Time}.csv', dtype= float, delimiter= ' ')
     Xmesh = np.genfromtxt(f'Nstest/Xpos_atTime-0.csv', dtype = float, delimiter=' ')
@@ -58,12 +57,9 @@ for Time in range(51):
         newPeaks = kmeans.cluster_centers_
         print([(c[0], c[1]) for c in newPeaks])
 
-
-
-
-    plt.contour(Xmesh, Ymesh, Vorticity_data, 200, cmap = 'turbo')
-    for c in newPeaks:
-        plt.plot(c[0], c[1], 'o', color = 'red')
+    plt.contour(Xmesh, Ymesh, Vorticity_data, 100, cmap = 'turbo')
+    #for c in newPeaks:
+        #plt.plot(c[0], c[1], 'o', color = 'red')
     #plt.plot([c[0]for c in Saddles], [c[1]for c in Saddles], 'o', color = 'green')
     plt.savefig(f'{_Directory}/testat{Time}.png')
     list_of_allPeaks.append(newPeaks)
